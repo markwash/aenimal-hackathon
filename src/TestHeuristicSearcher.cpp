6eb1be920d019cddd59ec239d35115a14ab4ca24
Mark Washenberger <mwashenb@gmail.com>
2010-09-13 14:17:41 -0400

    added HeuristicSearcher creation

--- src/TestHeuristicSearcher.cpp
+++ src/TestHeuristicSearcher.cpp
@@ -1,7 +1,9 @@
+#include "HeuristicSearcher.h"
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE TestHeuristicSearcher
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE ( empty )
+BOOST_AUTO_TEST_CASE(simple)
 {
+	HeuristicSearcher searcher = HeuristicSearcher();
 }
