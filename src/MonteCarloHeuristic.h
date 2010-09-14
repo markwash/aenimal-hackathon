f68b7d26117684eeb06e16aec42ede904399a1d4
Mark Washenberger <mwashenb@gmail.com>
2010-09-14 13:54:49 -0400

    add initial temperature

--- src/MonteCarloHeuristic.h
+++ src/MonteCarloHeuristic.h
@@ -1,2 +1,11 @@
 class MonteCarloHeuristic {
+	public:
+	MonteCarloHeuristic():
+		temperature(1.0)
+		{}
+
+	double getTemp(void) { return temperature; }
+
+	private:
+	double temperature;
 };
--- src/TestMonteCarloHeuristic.cpp
+++ src/TestMonteCarloHeuristic.cpp
@@ -1,11 +1,14 @@
+#include "MonteCarloHeuristic.h"
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE TestMonteCarloHeuristic
 #include <boost/test/unit_test.hpp>
 
 struct F {
 	F() {}
+	MonteCarloHeuristic heuristic;
 };
 
-BOOST_FIXTURE_TEST_CASE(null, F)
+BOOST_FIXTURE_TEST_CASE(get_temperature, F)
 {
+	BOOST_CHECK_EQUAL(heuristic.getTemp(), 1.0);
 }
