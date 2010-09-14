e166db4908ae5d9a8fa0bef1b497d4bc65ded78c
Mark Washenberger <mwashenb@gmail.com>
2010-09-14 16:10:43 -0400

    add an integration suite

---
+++ src/TestIntegration.cpp
@@ -0,0 +1,14 @@
+#include "SimulatedAnnealingHeuristic.h"
+#include <boost/random.hpp>
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestIntegration
+#include <boost/test/unit_test.hpp>
+
+
+struct SAnRandom {
+};
+
+BOOST_FIXTURE_TEST_CASE(null, SAnRandom)
+{
+}
