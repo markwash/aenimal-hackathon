560fc113d308e718461873f128cdd527ca3d7f56
Mark Washenberger <mwashenb@gmail.com>
2010-09-16 12:15:57 -0400

    add argument parsing to run-circles

--- src/run-circles.cpp
+++ src/run-circles.cpp
@@ -1,6 +1,8 @@
 #include <boost/random.hpp>
 #include <Magick++.h>
+#include <iostream>
 
+using namespace std;
 using namespace boost;
 using namespace Magick;
 
@@ -16,9 +18,22 @@ generator_type generator;
 uniform_real<> uni_dist(0, 1);
 rng uni(generator, uni_dist);
 
+char *parse_args(int argc, char **argv)
+{
+	if (argc < 2) {
+		cerr << "Usage: " << argv[0] << " <image file>" << endl;
+		exit(2);
+	}
+	return argv[1];
+}
+
 int main(int argc, char **argv)
 {
 	InitializeMagick(*argv);
+	char *target_image_file = parse_args(argc, argv);
+	Image target = Image(target_image_file);
 
 	return 0;
 }
+
+
